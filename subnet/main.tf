1bf0f0b6551df30c959447dd643aff8e08680f70
Shahlo0113 <shahlosuvonqulova@gmail.com>
2023-10-08 18:39:33 -0400

    subnets created

--- subnet/main.tf
+++ subnet/main.tf
@@ -26,11 +26,19 @@ data "aws_vpc" "main" {
   id = var.vpc_id
 }
 
+variable "subnets" {
+  type = map(object({
+    cidr_block = string
+  }))
+  default = {}
+}
+
 resource "aws_subnet" "main" {
+  for_each = var.subnets
   vpc_id     = data.aws_vpc.main.id
-  cidr_block = cidrsubnet(data.aws_vpc.main.cidr_block, 4, 1)
+  cidr_block = each.value.cidr_block#cidrsubnet(data.aws_vpc.main.cidr_block, 4, 1)
 
   tags = {
-    Name = "Main"
+    Name = each.key
   }
 }
